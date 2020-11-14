0de33a1f70f3cb62306e4fc5d99527f874513124
Ryan Whitmore <ryan@whitmorespace.com>
2020-11-14 09:54:38 -0800

    updated postgres config

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 module.exports = {
   migrationDirectory: 'migrations',
   driver: 'pg',
-  host: process.env.DATABASE_HOST,
-  port: process.env.DATABASE_PORT,
-  database: process.env.DATABASE_NAME,
-  username: process.env.DATABASE_USER,
+  connectionString:
+    process.env.NODE_ENV === 'test'
+      ? process.env.TEST_DATABASE_URL
+      : process.env.DATABASE_URL,
 };
